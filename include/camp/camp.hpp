dc148de9176371d7cd94130a965c61437bd1fcb9
Tom Scogland <scogland1@llnl.gov>
2018-09-05 22:22:58 -0700

    fix up copyright headers

--- include/camp/camp.hpp
+++ include/camp/camp.hpp
@@ -1,15 +1,12 @@
 /*
 Copyright (c) 2016-18, Lawrence Livermore National Security, LLC.
 Produced at the Lawrence Livermore National Laboratory
-Written by Tom Scogland <scogland1@llnl.gov>
-CODE-756261
-All rights reserved.
-
+Maintained by Tom Scogland <scogland1@llnl.gov>
+CODE-756261, All rights reserved.
 This file is part of camp.
 For details about use and distribution, please read LICENSE and NOTICE from
 http://github.com/llnl/camp
 */
-
 
 #ifndef __CAMP_HPP
 #define __CAMP_HPP
