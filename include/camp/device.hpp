ff977398ca817012e2114050ce76b7433ce766da
mdavis36 <davis291@llnl.gov>
2019-10-07 14:58:24 -0700

    hip integration first pass

--- include/camp/device.hpp
+++ include/camp/device.hpp
@@ -6,6 +6,11 @@
 #include <mutex>
 
 #include <cuda_runtime.h>
+
+#ifdef(CAMP_ENABLE_HIP)
+#include <hip/hip_runtime.hpp>
+#endif //#ifdef(CAMP_ENABEL_HIP)
+
 namespace camp
 {
 namespace devices
@@ -25,13 +30,26 @@ namespace devices
     public:
       CudaEvent(cudaStream_t stream){ 
 	cudaEventCreateWithFlags(&m_event, cudaEventDisableTiming); 
-	cudaEventRecord(m_event, stream); 
+	cudaEventRecord(m_event, stream);
       }
       bool check() const { return (cudaEventQuery(m_event) == cudaSuccess); }
       void wait() const { cudaEventSynchronize(m_event); }
     private:
       cudaEvent_t m_event;
   };
+
+#ifdef(CAMP_ENABLE_HIP)
+  class HipEvent
+  {
+    public:
+      HipEvent(hipStream_t stream){
+	hipEventCreateWithFlags(&m_event, hipEventDisableTiming);
+	hipEventRecord(m_event, stream);
+      }
+      bool check() const { return (hipEventQuery(m_event) == hipSuccess); }
+      void wait() const { hipEventSynchronize(m_event); }
+  };
+#endif //#ifdef(CAMP_ENABEL_HIP)
 
   class HostEvent
   {
@@ -160,6 +178,86 @@ namespace devices
   private:
     cudaStream_t stream;
   };
+
+#ifdef(CAMP_ENABLE_HIP)
+  class Hip
+  {
+    static hipStream_t get_a_stream(int num)
+    {
+      static hipStream_t streams[16] = {};
+      static int previous = 0;
+
+      static std::once_flag m_onceFlag;
+      static std::mutex m_mtx;
+
+      std::call_once(m_onceFlag,
+	[] {
+	  if (streams[0] == nullptr) {
+	    for (auto &s : streams) {
+	      hipStreamCreate(&s);
+	    }
+	  }
+	});
+
+      if (num < 0) {
+	m_mtx.lock();
+        previous = (previous + 1) % 16;
+	m_mtx.unlock();
+        return streams[previous];
+      }
+
+      return streams[num % 16];
+    }
+
+  public:
+    Hip(int device = 0, int group = -1) : stream(get_a_stream(group)) {}
+
+    // Methods
+    Platform get_platform() { return Platform::hip; }
+    static Hip &get_default()
+    {
+      static Hip h;
+      return h;
+    }
+    HipEvent get_event() {
+      return HipEvent(get_stream());
+    }
+    Event get_event_erased() {
+      return Event{HipEvent(get_stream())};
+    }
+    void wait() { hipStreamSynchronize(stream); }
+    void wait_on(Event *e) { e->wait(); }
+
+    // Memory
+    template <typename T>
+    T *allocate(size_t size)
+    {
+      T *ret = nullptr;
+      hipMallocManaged(&ret, sizeof(T) * size);
+      return ret;
+    }
+    void *calloc(size_t size)
+    {
+      void *p = allocate<char>(size);
+      this->memset(p, 0, size);
+      return p;
+    }
+    void free(void *p) { hipFree(p); }
+    void memcpy(void *dst, const void *src, size_t size)
+    {
+      hipMemcpyAsync(dst, src, size, hipMemcpyDefault, stream);
+    }
+    void memset(void *p, int val, size_t size)
+    {
+      hipMemsetAsync(p, val, size, stream);
+    }
+
+    hipStream_t get_stream() { return stream; }
+
+  private:
+    hipStream_t stream;
+  };
+#endif //#ifdef(CAMP_ENABEL_HIP)
 
   class Host
   {
