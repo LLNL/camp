4f645277bc48bc57a7752bafdc16e55feed15084
mdavis36 <davis291@llnl.gov>
2020-06-24 09:03:39 -0700

    cuda get_default w/ own cudaStream.

--- include/camp/resource/cuda.hpp
+++ include/camp/resource/cuda.hpp
@@ -82,7 +82,10 @@ namespace resources
       Platform get_platform() { return Platform::cuda; }
       static Cuda &get_default()
       {
-        static Cuda h(static_cast<cudaStream_t>(0));
+        static cudaStream_t s;
+        cudaStreamCreate(&s);
+
+        static Cuda h(s);
         return h;
       }
       CudaEvent get_event() { return CudaEvent(get_stream()); }
