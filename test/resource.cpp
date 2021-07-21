3d86d13896cc261edab48e148d25cd3d91c0f3e6
Jason Burmark <burmark1@llnl.gov>
2021-07-21 11:42:59 -0700

    Fix StreamSelect tests

--- test/resource.cpp
+++ test/resource.cpp
@@ -79,8 +79,8 @@ TEST(CampResource, StreamSelect)
   cudaStreamCreate(&stream1);
   cudaStreamCreate(&stream2);
 
-  Resource c1{CudaFromStream(stream1)};
-  Resource c2{CudaFromStream(stream2)};
+  Resource c1{Cuda::CudaFromStream(stream1)};
+  Resource c2{Cuda::CudaFromStream(stream2)};
 
   const int N = 5;
   int* d_array1 = c1.allocate<int>(5);
@@ -164,8 +164,8 @@ TEST(CampResource, StreamSelect)
   hipStreamCreate(&stream1);
   hipStreamCreate(&stream2);
 
-  Resource c1{Hip(stream1)};
-  Resource c2{Hip(stream2)};
+  Resource c1{Hip::HipFromStream(stream1)};
+  Resource c2{Hip::HipFromStream(stream2)};
 
   const int N = 5;
   int* d_array1 = c1.allocate<int>(5);
