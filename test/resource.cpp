72849990fe8fa239300a9e6918c5a96faeefc1e2
Jason Burmark <burmark1@llnl.gov>
2021-07-21 14:07:04 -0700

    Add a couple tests

--- test/resource.cpp
+++ test/resource.cpp
@@ -244,7 +244,16 @@ TEST(CampEventProxy, Get)
   }
 
   {
+    EventProxy<Host> ep{h1};
+    HostEvent e = ep;
+  }
+
+  {
     Event e = do_stuff(h1);
+  }
+
+  {
+    HostEvent e = do_stuff(h1);
   }
 
   {
@@ -254,6 +263,11 @@ TEST(CampEventProxy, Get)
   {
     EventProxy<Host> ep{h1};
     Event e = ep.get();
+  }
+
+  {
+    EventProxy<Host> ep{h1};
+    HostEvent e = ep.get();
   }
 }
 
