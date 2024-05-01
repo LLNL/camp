767c3842fb17a54161018750e3f415440e57745d
Alan Dayton <dayton8@llnl.gov>
2024-05-01 09:58:38 -0700

    Add implementation using integer sequences

--- include/camp/array.hpp
+++ include/camp/array.hpp
@@ -3,6 +3,7 @@
 
 #include "camp/defines.hpp"
 #include "camp/helpers.hpp"
+#include "camp/number.hpp"
 
 #include <cstddef>
 #include <stdexcept>
@@ -239,8 +240,39 @@ namespace camp {
       return move(a[I]);
    }
 
-   // TODO: Investigate the implementation from cppreference
-   //       with make_index_sequence
+#if defined(__cplusplus) && __cplusplus >= 201703L
+   // The use of auto for non-type template parameters is not supported
+   // until C++17
+   namespace detail {
+      template <class T, auto N, auto... I>
+      CAMP_HOST_DEVICE constexpr array<std::remove_cv_t<T>, N>
+         to_array_impl(T (&a)[N], idx_seq<I...>)
+      {
+         return {{a[I]...}};
+      }
+
+      template <class T, auto N, auto... I>
+      CAMP_HOST_DEVICE constexpr array<std::remove_cv_t<T>, N>
+         to_array_impl(T (&&a)[N], idx_seq<I...>)
+      {
+         return {{move(a[I])...}};
+      }
+   }
+ 
+   template <class T, auto N>
+   CAMP_HOST_DEVICE constexpr array<std::remove_cv_t<T>, N>
+      to_array(T (&a)[N])
+   {
+      return detail::to_array_impl(a, make_idx_seq_t<N>{});
+   }
+
+   template <class T, auto N>
+   CAMP_HOST_DEVICE constexpr array<std::remove_cv_t<T>, N>
+      to_array(T (&&a)[N])
+   {
+      return detail::to_array_impl(move(a), make_idx_seq_t<N>{});
+   }
+#else
    template <class T, std::size_t N>
    CAMP_HOST_DEVICE constexpr array<std::remove_cv_t<T>, N> to_array(T (&a)[N]) {
       array<std::remove_cv_t<T>, N> result;
@@ -262,6 +294,7 @@ namespace camp {
 
       return result;
    }
+#endif
 
 #if defined(__cplusplus) && __cplusplus >= 201703L
    ///
