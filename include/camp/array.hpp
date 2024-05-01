3f9c75b1c315d82519fa382c7e63de315b84f3c5
Alan Dayton <dayton8@llnl.gov>
2024-05-01 10:57:23 -0700

    Implement operators in terms of others

--- include/camp/array.hpp
+++ include/camp/array.hpp
@@ -187,19 +187,7 @@ namespace camp {
    template <class T, std::size_t N>
    CAMP_HOST_DEVICE constexpr bool operator>(const array<T, N>& lhs,
                                              const array<T, N>& rhs) {
-      for (std::size_t i = 0; i < N; ++i) {
-         if (lhs[i] > rhs[i]) {
-            return true;
-         }
-         else if (lhs[i] == rhs[i]) {
-            continue;
-         }
-         else {
-            return false;
-         }
-      }
-
-      return false;
+      return rhs < lhs;
    }
 
    template <class T, std::size_t N>
