07c7075239524c724cf88ecc491e853c00b1168d
Kristi Belcher <belcher6@llnl.gov>
2025-10-20 14:35:14 -0700

    adding more operators for host resources

--- include/camp/resource/host.hpp
+++ include/camp/resource/host.hpp
@@ -86,6 +86,38 @@ namespace resources
         return false;
       }
 
+      /*
+       * \return Since Host IDs are always zero, 0 < 0 is false
+       */
+      bool operator<(Host const&) const
+      {
+        return false;
+      }
+
+      /*
+       * \return Since Host IDs are always zero, 0 > 0 is false
+       */
+      bool operator>(Host const&) const
+      {
+        return false;
+      }
+
+      /*
+       * \return Since Host IDs are always zero, 0 <= 0 is true
+       */
+      bool operator<=(Host const&) const
+      {
+        return true;
+      }
+
+      /*
+       * \return Since Host IDs are always zero, 0 >= 0 is true
+       */
+      bool operator>=(Host const&) const
+      {
+        return true;
+      }
+
       size_t get_id() const
       {
         return 0; // All Host resources are the same
